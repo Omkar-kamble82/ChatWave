ec1a4990f158794778efc2479c55b855697f9ee4
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-03 18:19:23 +0530

    setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express.json())
+
+mongoose.set('strictQuery', true).connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to database')
+        app.listen(process.env.PORT, () => {
+            console.log('listening for requests on port', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+}) 
