83ad3808a96deb941bc007a6c88a4e3bc7f55f1c
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-05 20:43:44 +0530

    auth

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,46 +1,6 @@
-import { Button } from "@/components/ui/button"
-import { useUserContext } from "@/context/Authcontext"
-import toast from "react-hot-toast"
 import Navbar from "@/components/shared/Navbar"
+
 const Home = () => {
-
-  const { setValue, value } = useUserContext();
-  const logout = async () => {
-    try {
-        const response = await fetch(`${import.meta.env.VITE_SERVER_AUTH_URI}api/auth/logout`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        if (response.ok) {
-            toast.success("Logged out successfully")
-            setValue(null)
-            localStorage.removeItem("user");
-        }
-    } catch(err) {
-        console.log(err)
-    }
-  }
-
-  const getuser = async () => {
-    try {
-        const response = await fetch(`${import.meta.env.VITE_SERVER_AUTH_URI}api/auth/${value?._id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        const json = await response.json()
-        if (!response.ok) {
-            return
-        }
-        console.log(json)
-    } catch(err) {
-        console.log(err)
-    }
-}
-
   return (
     <div>
       <Navbar />
