261e088fcb0f6c514d29b0c641a959a820413e43
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-08 19:17:40 +0530

    json

--- frontend/src/context/Socketcontext.tsx
+++ frontend/src/context/Socketcontext.tsx
@@ -6,37 +6,43 @@ import io from "socket.io-client";
 const SocketContext = createContext();
 
 export const useSocketContext = () => {
-	return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
 // @ts-ignore
 export const SocketContextProvider = ({ children }) => {
-	const [socket, setSocket] = useState(null);
-	const [onlineUsers, setOnlineUsers] = useState([]);
-	const { value } = useUserContext();
-    // @ts-ignore
-	useEffect(() => {
-		if (value) {
-			const socket = io(import.meta.env.VITE_SERVER_AUTH_URI_LOCAL, {
-				query: {
-					userId: value._id,
-				},
-			});
-            // @ts-ignore
-			setSocket(socket);
-			socket.on("getOnlineUsers", (users) => {
-				setOnlineUsers(users);
-			});
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { value } = useUserContext();
 
-			return () => socket.close();
-		} else {
-			if (socket) {
-                // @ts-ignore
-				socket.close();
-				setSocket(null);
-			}
-		}
-	}, [value]);
+  const serverUrl = import.meta.env.VITE_SERVER_AUTH_URI;
 
-	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+  // @ts-ignore
+  useEffect(() => {
+    if (value) {
+      // Use the server URL variable
+      const socket = io(serverUrl, {
+        query: {
+          userId: value._id,
+        },
+      });
+
+      // @ts-ignore
+      setSocket(socket);
+
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        // @ts-ignore
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [value, serverUrl]);
+
+  return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
