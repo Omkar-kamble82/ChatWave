b93dc6fa6ebd732c95aace82a2239d22a3e7cfec
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-08 08:54:31 +0530

    server

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { login, logout, signup, getUsers } = require("../controllers/auth")
+const { getMessages, sendMessage, deleteMessage } = require("../controllers/message")
 
 const router = express.Router()
 
@@ -7,5 +8,9 @@ router.get("/:id", getUsers)
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
+
+router.post("/messages/:id", getMessages);
+router.patch("/delete/:id", deleteMessage);
+router.post("/send/:id", sendMessage);
 
 module.exports = router
