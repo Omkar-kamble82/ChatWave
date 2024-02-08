572c4c4dd24e1b136fa31c7e7d6ec85914e9d62b
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-08 17:26:37 +0530

    cors

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: "https://chatwave-c458d.web.app",
-		methods: ["GET", "POST"],
+		methods: ["GET", "POST", "PATCH"],
 	},
 });
 
