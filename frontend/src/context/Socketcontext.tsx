30ac50664f09a8f1e57761cfde6a5a20e3e844ca
Omkar-kamble82 <omkarsk82@gmail.com>
2024-02-09 15:37:11 +0530

    package.json

--- frontend/src/context/Socketcontext.tsx
+++ frontend/src/context/Socketcontext.tsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 	const [socket, setSocket] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
 	const { value } = useUserContext();
-	const serverUrl = import.meta.env.VITE_SERVER_AUTH_URI;
+	const serverUrl = import.meta.env.VITE_SERVER_AUTH_URI_LOCAL;
     // @ts-ignore
 	useEffect(() => {
 		if (value) {
