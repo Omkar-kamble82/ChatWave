a83a086e84382c8429f369696be156e7165c313d
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2025-11-02 15:00:47 +0530

    Update Sidebar.tsx

--- frontend/src/components/shared/Sidebar.tsx
+++ frontend/src/components/shared/Sidebar.tsx
@@ -31,7 +31,7 @@ const Sidebar = (props: props) => {
                     <span className={`flex gap-3 items-center hover:bg-[#32999f]/90 rounded-lg hover:text-white transition-all duration-500 font-semibold p-2 ${chat && chat.username === user.username ? "bg-[#32999f]/90" : "bg-transparent"}`}>
                         <img className="h-[34px] w-[34px] object-fill rounded-full" src={user.profilePic} alt={user.username} />
                         <p className="sm:text-[15px] md:text-[18px] text-grey-700">{user.username}</p>
-                        {onlineUsers.includes(user._id) && <span className="h-[8px] w-[8px] rounded-full bg-[#33aa33]" />}
+                        {onlineUsers.includes(user._id) ? <span className="h-[8px] w-[8px] rounded-full bg-[#33aa33]" /> : <span className="h-[8px] w-[8px] rounded-full bg-[#FF2C2C]" />}
                     </span>
                     {id === (users.length - 1 ) ?<></> : <hr/>}
                 </span>
